cabdb65e2317ff0307d56b607e5781f2e52fd8bf
MeysaMahfudhoh <116328545+MeysaMahfudhoh@users.noreply.github.com>
2024-05-07 01:22:46 +0700

    update

--- contact.php
+++ contact.php
@@ -95,7 +95,7 @@ if (isset($_SESSION['error'])) {
   <section class="contact" id="contact">
     <h1 class="heading">contact <span>us</span></h1>
     <div class="row">
-      <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d983.3235940970079!2d8.540719273659763!3d47.3713194174677!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47900a00aa1e1d17%3A0x278f576acdd580f5!2sStorchen%20Z%C3%BCrich%20-%20Lifestyle%20Boutique%20Hotel!5e0!3m2!1sde!2sch!4v1658505945506!5m2!1sde!2sch" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      <iframe class="map" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3159.148560217434!2d112.72421902653578!3d-7.314180581776443!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd7fb7a74c60bc1%3A0x3eaf6f55d501a7cd!2sFood%20Court%20Baseball%20Unesa!5e0!3m2!1sid!2sid!4v1714563856195!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       <form>
         <h3>get in touch</h3>
         <div class="inputBox">
